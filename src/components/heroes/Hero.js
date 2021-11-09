ca67eb63befe536a59c4b3a895fa8a274c4b0231
Claire Lemonnier <claire.lemonnier@pwc.com>
2021-11-09 13:59:38 +0000

    Connect route & api to hero page

--- src/components/heroes/Hero.js
+++ src/components/heroes/Hero.js
@@ -1,8 +1,33 @@
-import React, {useEffect, useContext, Fragment } from 'react';
+import React, {useEffect, useState, Fragment,  } from 'react';
+import Api from '../Api/Api';
+import '../../App.css';
+import { useParams } from 'react-router';
+const Hero = ({match}) => {
 
-const Hero = ({ match}) => {
-    return(
-        <h1>Hero page</h1>
-    )
+      // API Call
+  const [heroData, setHeroData] = useState([]);
+  const [ready, setReady] = useState(false);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const { data } = await Api.get(`/${id}`);
+      
+      setHeroData(data);
+      setReady(true);
+      
+    };
+    fetchApi();
+    console.log("api")
+    console.log(id)
+    console.log(heroData)
+  }, [id]);
+
+    return ready ? (
+       <Fragment>
+        <h1>Hero page {heroData.name}</h1>
+        </Fragment>
+    ) : null
 }
+
 export default Hero
